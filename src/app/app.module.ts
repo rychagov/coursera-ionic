908ec9ad53c40840a823b8120cd9679395d36b29
Andrey Rychagov <anryc@yandex.ru>
2019-03-15 21:07:18 +0300

    Network Status

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
 import { EmailComposer } from '@ionic-native/email-composer';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { Camera } from '@ionic-native/camera';
+import { Network } from '@ionic-native/network';
 import { IonicStorageModule } from '@ionic/storage';
 import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
@@ -72,6 +73,7 @@ import { baseURL } from '../shared/baseurl';
     EmailComposer,
     SocialSharing,
     Camera,
+    Network,
     DishProvider,
     LeaderProvider,
     PromotionProvider,
